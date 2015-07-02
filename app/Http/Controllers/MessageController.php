3134545dc865869c652feb789e9daaf65e396985
marrullas <marrullas@gmail.com>
2015-07-02 09:04:32 -0500

    correccion crear mensaje

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -55,7 +55,7 @@ class MessageController extends Controller
      *
      * @return Response
      */
-    public function create($id)
+    public function create()
     {
         //
         $respuesta =  false;
