3eeeecbcf46baf0792916ab71ea167619495e89e
marrullas <marrullas@gmail.com>
2015-07-03 15:44:23 -0500

    error componente carbon 2

--- config/app.php
+++ config/app.php
@@ -204,6 +204,7 @@ return [
 		'Validator' => 'Illuminate\Support\Facades\Validator',
 		'View'      => 'Illuminate\Support\Facades\View',
         'Carbon'    => 'Carbon\Carbon',
+        'carbon'    => 'Carbon\Carbon',
 
         /* para que funcione el provider forms html*/
 
