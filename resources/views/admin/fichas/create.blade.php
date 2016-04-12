37d69c4fed03e2985d90213449861ea464ffd8ae
marrullas <marrullas@gmail.com>
2016-04-12 14:20:43 +0000

    cambio de etiqueta acta-ficha

--- resources/views/admin/fichas/create.blade.php
+++ resources/views/admin/fichas/create.blade.php
@@ -7,14 +7,14 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Nueva Acta</div>
+                    <div class="panel-heading">Nueva Ficha</div>
 
                     <div class="panel-body">
                         @include('admin.partials.messages')
                         {!! Form::open(['route'=> 'admin.fichas.store', 'method' => 'POST' ]) !!}
                         @include('admin.fichas.partials.fields')
 
-                        <button type="submit" class="btn btn-default">Crear acta
+                        <button type="submit" class="btn btn-default">Crear Ficha
                         </button>
                         {!! Form::close() !!}
                     </div>
